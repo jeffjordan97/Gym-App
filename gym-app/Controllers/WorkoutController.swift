f9a7656f11edf9cb8e596a1e5439963501dd6b04
Jeffrey Jordan <jeffjordan97@hotmail.co.uk>
2020-03-05 19:33:00 +0000

    Added the following features: 	- tab bar controller 	- navigation controller 	- buttons linking to add goal view and add workout view Also refactored code: 	- Storyboards, separating into separate files. 	- view controllers for each storyboard view controller

---
+++ gym-app/Controllers/WorkoutController.swift
@@ -0,0 +1,34 @@
+//
+//  WorkoutController.swift
+//  gym-app
+//
+//  Created by Jordan, Jeffrey on 05/03/2020.
+//  Copyright © 2020 Jordan, Jeffrey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class WorkoutController: UIViewController {
+    
+    //MARK: Outlets
+    
+    //MARK: Attributes
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Workout Loaded")
+        
+    }
+}
