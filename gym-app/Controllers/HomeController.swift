aab54b6b0f7559703f89809eed57dc0335e8a501
Jeffrey Jordan <jeffjordan97@hotmail.co.uk>
2020-03-06 17:49:28 +0000

    Dropdown on button click - currently testing on Home Storyboard / Home Controller

--- gym-app/Controllers/HomeController.swift
+++ gym-app/Controllers/HomeController.swift
@@ -11,15 +11,34 @@ import UIKit
 import CoreData
 import DropDown
 
+
 class HomeController: UIViewController {
     
     //MARK: Outlets
     
+    @IBOutlet weak var typeDrop: UIView!
     
     
     //MARK: Attributes
     
+    
+    @IBAction func typePick(_ sender: Any) {
+         let dropDown = DropDown()
+        
+        // The list of items to display. Can be changed dynamically
+        dropDown.dataSource = ["Car", "Motorcycle", "Truck"]
+        
+        // The view to which the drop down will appear on
+        dropDown.anchorView = typeDrop // UIView or UIBarButtonItem
 
+        
+        // Action triggered on selection
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+          print("Selected item: \(item) at index: \(index)")
+        }
+        
+        dropDown.show()
+    }
     
     
     //MARK: viewDidLoad
@@ -30,23 +49,17 @@ class HomeController: UIViewController {
         print("Home Loaded")
         
         
-        let dropDown = DropDown()
+       
 
-        // The view to which the drop down will appear on
-        //dropDown.anchorView = typeDrop // UIView or UIBarButtonItem
-
-        // The list of items to display. Can be changed dynamically
-        dropDown.dataSource = ["Car", "Motorcycle", "Truck"]
         
-        // Action triggered on selection
-        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-          print("Selected item: \(item) at index: \(index)")
-        }
+        
+        
+        
 
         // Will set a custom width instead of the anchor view width
         //dropDownLeft.width = 200
         
-        //dropDown.show()
+        //
         
         
         
