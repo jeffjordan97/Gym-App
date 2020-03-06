3dbaa6320fae0a97c6497dd1ee22a0d151bd6c16
Jeffrey Jordan <jeffjordan97@hotmail.co.uk>
2020-03-06 18:46:10 +0000

    Added Exercises View Controller and linked to add button from Add Workouts page.

---
+++ gym-app/Controllers/ExercisesController.swift
@@ -0,0 +1,34 @@
+//
+//  ExercisesController.swift
+//  gym-app
+//
+//  Created by Jordan, Jeffrey on 06/03/2020.
+//  Copyright © 2020 Jordan, Jeffrey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ExercisesController: UIViewController {
+    
+    //MARK: Outlets
+    
+    //MARK: Attributes
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Exercises Loaded")
+        
+    }
+}
